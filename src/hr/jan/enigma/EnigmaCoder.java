a04fb763bb198cbe075154d1e123a179aefdd007
Jan Komericki <jan.komericki@gmail.com>
2024-07-25 19:51:33 +0200

    User-friendliness Improvements
    Mainly just baby-proofing the program, making it easier to use. Not finished yet.

--- src/hr/jan/enigma/EnigmaCoder.java
+++ src/hr/jan/enigma/EnigmaCoder.java
@@ -6,20 +6,38 @@ public class EnigmaCoder {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Please enter your super secret message that you'd like to encode:");
-		String message = sc.nextLine();
-		String[] swaps = {"AR", "GK", "OX"};
+		System.out.println("Welcome to Enigma.\n"
+				+ "Before use please read the README.md file.\n"
+				+ "Setup is required before you can start encrypting and decrypting messages.\n"
+				+ "Please enter the letters you would like to swap in the Plugboard, as pairs separated by spaces (AB CD EF):");
+		String[] swaps = sc.nextLine().split(" "); //AR GK OX
 		Enigma e = new Enigma(swaps);
 		
-		e.setKey("DOG");
-		e.setRings(1, 1, 2);
+		System.out.println("Please enter the Key of your encrpted message:");
+		String key = sc.nextLine();
+		e.setKey(key); //DOG
 		
+		System.out.println("Please enter the offset of your rings, as numbers separated by a space (1 1 2).\n"
+				+ "Note-Numbers can only be between 1 and 26, with 1 meaning no change in offset of rings.\n"
+				+ "Enter numbers here:");
+		String[] numbers = sc.nextLine().split(" ");
+		e.setRings(Integer.parseInt(numbers[0]), Integer.parseInt(numbers[1]), Integer.parseInt(numbers[2]));
+		
+		System.out.println("Setup succesful! You may now enter messages to encipher them, or enter enciphered ones to decipher them.\nTo exit the program, enter \\ESCAPE\\.");
+		
+		while(true) {
+		System.out.print("Please enter your super secret message that you'd like to encode:");
+		String message = sc.nextLine();
+		if(message.equals("\\ESCAPE\\")) break;
 		String output = "";
 		String[] s = message.split("");
 		for(String s1 : s) {
 			output = output + e.encipher(s1);
 		}
 		System.out.println("Your enciphered message reads: " + output);
+		}
+		
+		System.out.println("Thank you for using Enigma. Have a nice day :)");
 	}
 
 }
